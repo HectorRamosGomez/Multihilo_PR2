9a75356402ce2bb2b937d850d58e7b777838a967
Hector Ramos Gomez <hectorramosgomez717@gmail.com>
2026-03-09 18:39:21 +0100

    propiedades de el JFrame

---
+++ procesos/src/main/java/procesos_evaluable2/ExploradorArchivos.java
@@ -0,0 +1,15 @@
+package procesos_evaluable2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class ExploradorArchivos extends JFrame {
+    private JTextArea textArea;
+    private JButton btnFinalizar;
+    private Thread hiloEscaneo;
+    private volatile boolean detenido = false;
+
+    
+
+}
