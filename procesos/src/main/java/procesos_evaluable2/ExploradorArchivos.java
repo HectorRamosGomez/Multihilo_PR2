9e34cc08c3de474053a064abc649f2bcc9f5503c
Hector Ramos Gomez <hectorramosgomez717@gmail.com>
2026-03-09 18:43:45 +0100

    Logica de la ventana

--- procesos/src/main/java/procesos_evaluable2/ExploradorArchivos.java
+++ procesos/src/main/java/procesos_evaluable2/ExploradorArchivos.java
@@ -10,6 +10,37 @@ public class ExploradorArchivos extends JFrame {
     private Thread hiloEscaneo;
     private volatile boolean detenido = false;
 
-    
+    public ExploradorArchivos() {
+
+        //Titulo de la ventana
+        setTitle("Escáner de Unidad C:");
+        //Tamaño de la ventana
+        setSize(600, 400);
+        //Cuando cierres el programa se cerrara
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //Organizador de componentes
+        setLayout(new BorderLayout());
+
+        //Crear la pantalla en blanco para que se pueda escribir
+        textArea = new JTextArea();
+        //Evita que el usuario pueda escribir en la pantalla
+        textArea.setEditable(false);
+        //Agrega una barra de desplazamiento a la pantalla
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        //Agrega la pantalla con la barra de desplazamiento a la ventana
+        add(scrollPane, BorderLayout.CENTER);
+
+        //Boton para finalizar el programa
+        btnFinalizar = new JButton("Finalizar Programa");
+        btnFinalizar.addActionListener(e -> {
+            detenido = true;
+            System.exit(0);
+        });
+        //Agrega el boton a la ventana
+        add(btnFinalizar, BorderLayout.SOUTH);
+
+        //Hilo para escanear los archivos
+        iniciarEscaneo();
+    }
 
 }
